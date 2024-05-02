8d5eb7247026e5a10386f55940331d1778f6097c
BelmaDedic <belma.dedic2019@size.ba>
2024-05-02 16:51:11 +0200

    modal over the screen

--- src/components/products-view/product-view-dialog.tsx
+++ src/components/products-view/product-view-dialog.tsx
@@ -1,5 +1,5 @@
+import React, { useState } from "react";
 import Grid from "@mui/material/Grid";
-import Rating from "@mui/material/Rating";
 import Dialog from "@mui/material/Dialog";
 import Button from "@mui/material/Button";
 import Divider from "@mui/material/Divider";
@@ -30,6 +30,8 @@ interface Props {
 
 export default function ProductViewDialog(props: Props) {
   const { product, openDialog, handleCloseDialog } = props;
+  const [openImageDialog, setOpenImageDialog] = useState(false); // State for opening image dialog
+  const [imageIndex, setImageIndex] = useState(0); // State to track current image index
 
   const { state, dispatch } = useCart();
   const cartItem = state.cart.find((item) => item.id === product.id);
@@ -49,38 +51,64 @@ export default function ProductViewDialog(props: Props) {
         category: product.category,
         price: product.price,
         slug: product.slug,
-      }
+      },
     });
   };
 
+  const handleImageClick = (index: number) => {
+    setImageIndex(index);
+    setOpenImageDialog(true);
+  };
+
+  const handleCloseImageDialog = () => {
+    setOpenImageDialog(false);
+  }
+
   return (
-    <Dialog
-      open={openDialog}
-      maxWidth={false}
-      onClose={handleCloseDialog}
-      sx={{ zIndex: 1501 }}
-    >
-      <DialogContent sx={{ maxWidth: 900, width: "100%" }}>
-        <div style={{cursor: "default"}}>
-          <Grid container spacing={3}>
-            <Grid item md={6} xs={12}>
-              {/* Conditionally render the Carousel component */}
-              {product.images && product.images.length > 1 ? (
-                <Carousel
-                  slidesToShow={1}
-                  arrowStyles={{
-                    boxShadow: 0,
-                    color: "secondary.main",
-                    backgroundColor: "white",
-                    fontWeight: "bolder"
-                  }}
-                >
-                  {/* Mapping through product images */}
-                  {product.images.map((image, index) => (
+    <>
+      <Dialog
+        open={openDialog}
+        maxWidth={false}
+        onClose={handleCloseDialog}
+        sx={{ zIndex: 1501 }}
+      >
+        <DialogContent sx={{ maxWidth: 900, width: "100%" }}>
+          <div style={{ cursor: "default" }}>
+            <Grid container spacing={3}>
+              <Grid item md={6} xs={12}>
+                {/* Conditionally render the Carousel component */}
+                {product.images && product.images.length > 1 ? (
+                  <Carousel
+                    slidesToShow={1}
+                    arrowStyles={{
+                      boxShadow: 0,
+                      color: "secondary.main",
+                      backgroundColor: "white",
+                      fontWeight: "bolder",
+                    }}
+                  >
+                    {/* Mapping through product images */}
+                    {product.images.map((image, index) => (
+                      <div key={index} onClick={() => handleImageClick(index)}>
+                        <BazaarImage
+                          src={image.imageUrl}
+                          alt={`product-${index}`}
+                          sx={{
+                            mx: "auto",
+                            width: "100%",
+                            objectFit: "contain",
+                            height: { sm: 400, xs: 250 },
+                          }}
+                        />
+                      </div>
+                    ))}
+                  </Carousel>
+                ) : (
+                  // Render single image without Carousel
+                  product.images && product.images.length === 1 && (
                     <BazaarImage
-                      key={index}
-                      src={image.imageUrl}
-                      alt={`product-${index}`}
+                      src={product.images[0].imageUrl}
+                      alt={`product-0`}
                       sx={{
                         mx: "auto",
                         width: "100%",
@@ -88,118 +116,211 @@ export default function ProductViewDialog(props: Props) {
                         height: { sm: 400, xs: 250 },
                       }}
                     />
-                  ))}
-                </Carousel>
-              ) : (
-                // Render single image without Carousel
-                product.images && product.images.length === 1 && (
-                  <BazaarImage
-                    src={product.images[0].imageUrl}
-                    alt={`product-0`}
-                    sx={{
-                      mx: "auto",
-                      width: "100%",
-                      objectFit: "contain",
-                      height: { sm: 400, xs: 250 },
-                    }}
-                  />
-                )
-              )}
-            </Grid>
+                  )
+                )}
+              </Grid>
 
-            <Grid item md={6} xs={12} alignSelf="center">
-              <H2>
-                {product.brand} {product.model}
-              </H2>
-              {product.category && (
+              <Grid item md={6} xs={12} alignSelf="center">
+                <H2>
+                  {product.brand} {product.model}
+                </H2>
+                {product.category && (
+                  <Paragraph
+                    py={1}
+                    color="grey.500"
+                    fontWeight={600}
+                    fontSize={13}
+                  >
+                    KATEGORIJA: {product.category.name}
+                  </Paragraph>
+                )}
+
+                <H1 color="primary.main">{product.price}KM</H1>
+
                 <Paragraph
                   py={1}
                   color="grey.500"
                   fontWeight={600}
                   fontSize={13}
                 >
-                  KATEGORIJA: {product.category.name}
+                  STANJE: {product.productStatus.status}
                 </Paragraph>
-              )}
 
-              <H1 color="primary.main">{product.price}KM</H1>
+                <Paragraph my={2}>{product.description}</Paragraph>
 
-              <Paragraph py={1} color="grey.500" fontWeight={600} fontSize={13}>
-                STANJE: {product.productStatus.status}
-              </Paragraph>
+                <Divider sx={{ mb: 2 }} />
 
-              <Paragraph my={2}>{product.description}</Paragraph>
-
-              <Divider sx={{ mb: 2 }} />
-
-              {!cartItem?.qty && product.stock > 0 && (
-                <Button
-                  size="large"
-                  color="primary"
-                  variant="contained"
-                  onClick={handleCartAmountChange(1)}
-                  sx={{ height: 45 }}
-                >
-                  Dodaj u korpu
-                </Button>
-              )}
-
-              {product.stock === 0 &&
-              <Button 
-                variant="contained" 
-                disabled
-              >
-                Nema na stanju
-              </Button>
-              }
-
-            {cartItem?.qty > 0 ?            
-                <FlexBox alignItems="center">
-                  <Button
-                    size="small"
-                    color="primary"
-                    variant="outlined"
-                    sx={{ p: ".6rem", height: 45 }}
-                    onClick={handleCartAmountChange(cartItem?.qty - 1)}
-                  >
-                    <Remove fontSize="small" />
-                  </Button>
-
-                  <H3 fontWeight="600" mx={2.5}>
-                    {cartItem?.qty.toString().padStart(2, "0")}
-                  </H3>
-
-                  { cartItem?.qty  == cartItem.stock ?
-                    <Button 
-                      variant="contained" 
-                      disabled
-                      style={{width: "50px", height: "50px", fontSize: "8px", fontWeight: "bold"}}
+                {!cartItem?.qty && product.stock > 0 && (
+                  <>
+                    <Button
+                      size="large"
+                      color="primary"
+                      variant="contained"
+                      onClick={handleCartAmountChange(1)}
+                      sx={{ height: 45 }}
                     >
-                    Maksimum
-                    </Button> : 
+                      Dodaj u korpu
+                    </Button>
+                    <Button
+                      size="large"
+                      color="secondary"
+                      variant="outlined"
+                      onClick={handleCloseDialog}
+                      sx={{ marginLeft: "2%" }}
+                    >
+                      Zatvori prozor
+                    </Button>
+                  </>
+                )}
+
+                {product.stock === 0 && (
+                  <>
+                    <Button variant="contained" disabled>
+                      Nema na stanju
+                    </Button>
+                    <Button
+                      size="large"
+                      color="secondary"
+                      variant="outlined"
+                      onClick={handleCloseDialog}
+                      sx={{ marginLeft: "2%" }}
+                    >
+                      Zatvori prozor
+                    </Button>
+                  </>
+                )}
+
+                {cartItem?.qty > 0 && (
+                  <FlexBox alignItems="center">
                     <Button
                       size="small"
                       color="primary"
                       variant="outlined"
                       sx={{ p: ".6rem", height: 45 }}
-                      onClick={handleCartAmountChange(cartItem?.qty + 1)}
-                    >          
-                      <Add fontSize="small" />
+                      onClick={handleCartAmountChange(cartItem?.qty - 1)}
+                    >
+                      <Remove fontSize="small" />
                     </Button>
-                  }
-                </FlexBox>
-            : ""}
-            </Grid>
-          </Grid>
-        </div>
 
-        <IconButton
-          sx={{ position: "absolute", top: 3, right: 3 }}
-          onClick={handleCloseDialog}
-        >
-          <Close fontSize="small" color="secondary" />
-        </IconButton>
-      </DialogContent>
-    </Dialog>
+                    <H3 fontWeight="600" mx={2.5}>
+                      {cartItem?.qty.toString().padStart(2, "0")}
+                    </H3>
+
+                    {cartItem?.qty == cartItem.stock ? (
+                      <Button
+                        variant="contained"
+                        disabled
+                        style={{
+                          width: "50px",
+                          height: "50px",
+                          fontSize: "8px",
+                          fontWeight: "bold",
+                        }}
+                      >
+                        Maksimum
+                      </Button>
+                    ) : (
+                      <Button
+                        size="small"
+                        color="primary"
+                        variant="outlined"
+                        sx={{ p: ".6rem", height: 45 }}
+                        onClick={handleCartAmountChange(cartItem?.qty + 1)}
+                      >
+                        <Add fontSize="small" />
+                      </Button>
+                    )}
+                    <Button
+                      size="large"
+                      color="secondary"
+                      variant="outlined"
+                      onClick={handleCloseDialog}
+                      sx={{ marginLeft: "2%" }}
+                    >
+                      Zatvori prozor
+                    </Button>
+                  </FlexBox>
+                )}
+              </Grid>
+            </Grid>
+          </div>
+
+          <IconButton
+            sx={{ position: "absolute", top: 3, right: 3 }}
+            onClick={handleCloseDialog}
+          >
+            <Close fontSize="small" color="secondary" />
+          </IconButton>
+        </DialogContent>
+      </Dialog>
+
+      {/* New dialog for displaying image carousel */}
+      <Dialog
+        open={openImageDialog}
+        onClose={handleCloseImageDialog}
+        fullScreen
+        sx={{ zIndex: 1502 }} // Ensure the image carousel dialog appears above the main dialog
+      >
+        <DialogContent>
+          {/* Conditionally render the Carousel component */}
+          {product.images && product.images.length > 1 ? (
+                  <Carousel
+                    slidesToShow={1}
+                    arrowStyles={{
+                      boxShadow: 0,
+                      color: "secondary.main",
+                      backgroundColor: "white",
+                      fontWeight: "bolder",
+                    }}
+                  >
+                    {/* Mapping through product images */}
+                    {product.images.map((image, index) => (
+                      <div key={index} onClick={() => handleImageClick(index)}>
+                        <BazaarImage
+                          src={image.imageUrl}
+                          alt={`product-${index}`}
+                          sx={{
+                            mx: "auto",
+                            width: "85vw",
+                            objectFit: "fill",
+                            height: { sm: "83.5vh", xs: 250 },
+                          }}
+                        />
+                      </div>
+                    ))}
+                  </Carousel>
+                ) : (
+                  // Render single image without Carousel
+                  product.images && product.images.length === 1 && (
+                    <BazaarImage
+                      src={product.images[0].imageUrl}
+                      alt={`product-0`}
+                      sx={{
+                        mx: "auto",
+                        width: "100%",
+                        objectFit: "contain",
+                        height: { sm: "83.5vh", xs: 250 },
+                      }}
+                    />
+                  )
+                )}
+          <Button
+            size="large"
+            color="secondary"
+            variant="outlined"
+            onClick={handleCloseImageDialog}
+            sx={{ display: 'block', margin: "auto" }}
+          >
+            Zatvori prozor
+          </Button>
+          <IconButton
+            sx={{ position: "absolute", top: 3, right: 3 }}
+            onClick={handleCloseImageDialog}
+          >
+            <Close fontSize="small" color="secondary" />
+          </IconButton>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
